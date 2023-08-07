878e08b84090be5db54cef8becb526f0c1dd967a
Mohammadreza <Mohammadrezaaalaei@gmail.com>
2023-08-08 00:22:28 +0330

    reverting previous setting

--- src/experience.jsx
+++ src/experience.jsx
@@ -5,38 +5,38 @@ import {useFrame} from "@react-three/fiber"
 import * as THREE from 'three'
 const point = [-1, 2.3, -2.1]
 const point2 = [-1, 2.3, -2.6]
-const plane = new THREE.PlaneGeometry(2,2,2,2)
+// const plane = new THREE.PlaneGeometry(2,2,2,2)
 const Experience = () => {
     const pointLightRef = useRef(null)
     const pointLightRef2 = useRef(null)
-    useFrame( (state) => {
-        if (!! pointLightRef.current) {
-            pointLightRef.current.shadow.bias = -0.0001
-            pointLightRef.current.shadow.mapSize.width = 256
-            pointLightRef.current.shadow.mapSize.height = 256
-            // pointLightRef.current.shadow.camera.near = 0.5
-            // pointLightRef.current.shadow.camera.far = 100
-            // pointLightRef.current.shadow.camera.left = -10
-            // pointLightRef.current.shadow.camera.right = 10
-            // pointLightRef.current.shadow.camera.top = 10
-            // pointLightRef.current.shadow.camera.bottom = -10
-            pointLightRef.current.shadow.radius = 5
-            pointLightRef.current.shadow.blurSamples = 25
-        }
-               if (!! pointLightRef2.current) {
-            pointLightRef2.current.shadow.bias = -0.0001
-            pointLightRef2.current.shadow.mapSize.width = 256
-            pointLightRef2.current.shadow.mapSize.height = 256
-            // pointLightRef.current.shadow.camera.near = 0.5
-            // pointLightRef.current.shadow.camera.far = 100
-            // pointLightRef.current.shadow.camera.left = -10
-            // pointLightRef.current.shadow.camera.right = 10
-            // pointLightRef.current.shadow.camera.top = 10
-            // pointLightRef.current.shadow.camera.bottom = -10
-            pointLightRef2.current.shadow.radius = 5
-            pointLightRef2.current.shadow.blurSamples = 25
-        }
-    })
+//     useFrame( (state) => {
+//         if (!! pointLightRef.current) {
+//             pointLightRef.current.shadow.bias = -0.00001
+//             pointLightRef.current.shadow.mapSize.width = 256
+//             pointLightRef.current.shadow.mapSize.height = 256
+//             // pointLightRef.current.shadow.camera.near = 0.5
+//             // pointLightRef.current.shadow.camera.far = 100
+//             // pointLightRef.current.shadow.camera.left = -10
+//             // pointLightRef.current.shadow.camera.right = 10
+//             // pointLightRef.current.shadow.camera.top = 10
+//             // pointLightRef.current.shadow.camera.bottom = -10
+//             pointLightRef.current.shadow.radius = 5
+//             pointLightRef.current.shadow.blurSamples = 25
+//         }
+//                if (!! pointLightRef2.current) {
+//             pointLightRef2.current.shadow.bias = -0.0001
+//             pointLightRef2.current.shadow.mapSize.width = 256
+//             pointLightRef2.current.shadow.mapSize.height = 256
+//             // pointLightRef.current.shadow.camera.near = 0.5
+//             // pointLightRef.current.shadow.camera.far = 100
+//             // pointLightRef.current.shadow.camera.left = -10
+//             // pointLightRef.current.shadow.camera.right = 10
+//             // pointLightRef.current.shadow.camera.top = 10
+//             // pointLightRef.current.shadow.camera.bottom = -10
+//             pointLightRef2.current.shadow.radius = 5
+//             pointLightRef2.current.shadow.blurSamples = 25
+//         }
+//     })
 
     return (
         <>
