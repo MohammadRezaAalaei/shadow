44d73cd0f156f5fbe5d97f2190b5b56aee548975
Mohammadreza <Mohammadrezaaalaei@gmail.com>
2023-08-07 20:53:15 +0330

    modifying shadows

--- src/experience.jsx
+++ src/experience.jsx
@@ -10,15 +10,15 @@ const Experience = () => {
     const pointLightRef = useRef(null)
     useFrame( (state) => {
         if (!! pointLightRef.current) {
-            pointLightRef.current.shadow.bias = -0.0001
-            pointLightRef.current.shadow.mapSize.width = 1024*2
-            pointLightRef.current.shadow.mapSize.height = 1024*2
-            pointLightRef.current.shadow.camera.near = 0.5
-            pointLightRef.current.shadow.camera.far = 25
-            pointLightRef.current.shadow.camera.left = -10
-            pointLightRef.current.shadow.camera.right = 10
-            pointLightRef.current.shadow.camera.top = 10
-            pointLightRef.current.shadow.camera.bottom = -10
+            pointLightRef.current.shadow.bias = -0.00001
+            pointLightRef.current.shadow.mapSize.width = 512
+            pointLightRef.current.shadow.mapSize.height = 512
+            // pointLightRef.current.shadow.camera.near = 0.5
+            // pointLightRef.current.shadow.camera.far = 100
+            // pointLightRef.current.shadow.camera.left = -10
+            // pointLightRef.current.shadow.camera.right = 10
+            // pointLightRef.current.shadow.camera.top = 10
+            // pointLightRef.current.shadow.camera.bottom = -10
             pointLightRef.current.shadow.radius = 5
             pointLightRef.current.shadow.blurSamples = 25
         }
@@ -26,7 +26,7 @@ const Experience = () => {
     return (
         <>
         <ambientLight intensity={0.3}/>
-        <pointLight intensity={1} position={point} castShadow={true} ref={pointLightRef}/>
+        <pointLight intensity={0.5} position={point} castShadow={true} ref={pointLightRef}/>
         {/* <mesh position={point} castShadow={true}> 
         <sphereGeometry args={[0.2, 32, 32]}/>
         <meshPhongMaterial color="white"/>
